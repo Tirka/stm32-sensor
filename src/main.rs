9a0df42c4af73075b77299e33a2b228d578b65e7
Maksim Vykhota <m.vykhota@protonmail.com>
2021-06-23 14:18:47 +0300

    add defmt to project

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,11 @@
 #![no_std]
 #![no_main]
 
-// pick a panicking behavior
-use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-// use panic_abort as _; // requires nightly
-// use panic_itm as _; // logs messages over ITM; requires ITM support
-// use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
+use defmt_rtt as _;
+use panic_probe as _;
 
 use cortex_m_rt::entry;
 use stm32f4xx_hal as hal;
-// use hal::prelude::*;
 
 #[entry]
 fn main() -> ! {
